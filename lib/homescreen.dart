4c162bde81438f73fc44b83159a9227c879bed23
Hassan Sultan Khan <hassansultankhan@gmail.com>
2023-02-27 00:53:42 +0500

    everything has a start and its a journey"

---
+++ lib/homescreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class homescreen extends StatefulWidget {
+  const homescreen({Key? key}) : super(key: key);
+
+  @override
+  State<homescreen> createState() => _homescreenState();
+}
+
+class _homescreenState extends State<homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:app_1/homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Bismillah",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme(
+           primary: Color.fromARGB(255, 171, 223, 60),// for inside containers
+          secondary: Color.fromARGB(255, 87, 188, 24),//text
+          surface: Color.fromARGB(255, 210, 227, 194),
+          background: Color.fromARGB(255, 255, 255, 255),
+          error: Color.fromARGB(255, 251, 3, 3),
+          onPrimary: Color.fromARGB(255, 4, 85, 0),
+          onSecondary: Color.fromARGB(255, 0, 0, 0),
+          onSurface: Colors.grey,
+          onBackground: Colors.grey,
+          onError: Color.fromARGB(255, 255, 1, 1),
+          brightness: Brightness.light,
+      ),
+    ),
+    home: homescreen()
+    ),
+  );
+}
