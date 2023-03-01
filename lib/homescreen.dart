3c1c3da50973bd824cb0064fe73024258405ea02
Hassan Sultan Khan <hassansultankhan@gmail.com>
2023-03-02 01:51:34 +0500

    02/3/23

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class homescreen extends StatefulWidget {
   const homescreen({Key? key}) : super(key: key);
@@ -12,7 +10,65 @@ class homescreen extends StatefulWidget {
 class _homescreenState extends State<homescreen> {
   @override
   Widget build(BuildContext context) {
+    
+    
+    List<university> unidata =[];
+    //initializations
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Some API"),
+        centerTitle: true,
+        elevation: 5.0,
+      ),
+
+      body: Container(
+        child: Column(
+          children: [
+            ListView.builder(
+              //item count not add yet
+              // inkwell
+              //single child scroll view
+              itemBuilder: ((context, index) {
+                return Column(
+                  children: [
+                    Padding(padding: EdgeInsets.fromLTRB(5, 10, 0, 10),
+                    
+                    child: Row(
+                      //Axis alignment
+                      children: [
+                        Container(
+                        width: MediaQuery.of(context).size.width,
+                        alignment:  Alignment.centerLeft,
+                        child: Padding(padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('${unidata[index].state}',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black54
+                            ),
+                            ),
+                          ],
+
+                        ),
+                        
+                        ),
+                        )
+                      ],
+
+                    ),
+                    )
+
+                  ],
+                );
+              })
+            )
+          ],
+
+        ),
+      ),
 
     );
   }
